b3e0bbd507f7ac92fc5d5868ea9406efddb16d46
NathaliaLeandroFerreira <naleestudo@gmail.com>
2026-08-11 13:48:54 -0400

    Update Tarefa2SQL.sql

--- Tarefa2SQL.sql
+++ Tarefa2SQL.sql
@@ -34,3 +34,6 @@ select nome,count(Servico.preco) as QuantidadeTotalDeServico from Pessoa left jo
 /* consulta 5 - os 2 maiores prestadores de serviço,usando o limit para limitar a quantidade de linhas que aparecem*/
 
 select nome,count(Servico.preco) as QuantidadeTotalDeServico from Pessoa left join Servico on Pessoa.id = Servico.id_pessoa group by Pessoa.nome order by count(Servico.preco) desc limit 2 ;
+/*consulta 6- ordenado pela media aritmetica em ordem descendente,pessoas que nunca prestaram nenhum serviço tem 0 ao inves de NULL.  */
+
+select nome,coalesce(avg(Servico.preco),0) as Media from Pessoa left join Servico on Pessoa.id= Servico.id_Pessoa group by Pessoa.nome order by avg(Servico.preco) desc
